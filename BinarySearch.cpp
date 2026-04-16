6d772001ebd2667ff0beea0d9387b3179004e519
sabrinakayla <sabrinakuliah10@gmail.com>
2026-04-16 10:52:46 +0700

    menambahkan display

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -51,5 +51,17 @@ void bubbleSortArray()
     } while (pass <= nPanjang -1 );
 }
 
+void display()
+{
+    for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+        {
+            cout << " -> ";
+        }
+    }
+    cout << endl;
+}
 
 
